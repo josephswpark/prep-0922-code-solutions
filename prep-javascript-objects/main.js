f6b821aefe24ed66f298aa8ffcc1c5ff378f5cc2
Joseph Park <parkjoseph54@gmail.com>
2022-09-28 01:28:08 +0000

    Completed prep-javascript-objects exericse.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,17 @@
+const person = {
+  firstName: 'Joseph',
+  lastName: 'Park',
+  hobby: 'basketball',
+  job: 'Medical Assistant',
+  previousJob: 'Student'
+};
+console.log(person);
+
+const fullName = 'Joseph' + ' Park';
+console.log("The person's name is:", fullName);
+
+console.log("The person's current job is:", person.job);
+
+console.log("The person's previous job is:", person.previousJob);
+
+console.log(person);
